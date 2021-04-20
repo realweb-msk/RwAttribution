f932ee297bde8ae001df63794bd475cf85f05f06
FreedomSlow <60659176+FreedomSlow@users.noreply.github.com>
2021-04-20 10:24:09 +0300

    Update README.md

--- tests/markov_test.py
+++ tests/markov_test.py
@@ -3,15 +3,9 @@ import pandas as pd
 
 
 data_1 = pd.read_csv('../data/attribution data.csv', sep=',')
-# data_2 = pd.read_csv('data_attrib_AB_credit-card.csv', sep=',', converters={'client_id' : str})
 
 markov_1 = RwMarkov(data_1, 'channel', 'cookie', 'time', 'conversion', verbose=1)
-# markov_1 = RwMarkov(data_2, 'channel_group', 'client_id', 'visit_start_time', 'session_with_conversion', verbose=1)
-# print(markov_2.make_markov(250000, 0.09))
 
-
-# conv = markov_1.conversions_per_channel(prep)
-# print(markov_1.removal_effect(matr))#, conv))
 print(markov_1.make_markov())
 
 
