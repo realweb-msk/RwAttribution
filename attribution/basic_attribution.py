67e485e4da607efcd71b02f9a6a8e1594860f3cf
FreedomSlow <60659176+FreedomSlow@users.noreply.github.com>
2021-05-08 14:24:13 +0300

    Uniform column naming in all attributions. Last non-direct model for general case

--- attribution/basic_attribution.py
+++ attribution/basic_attribution.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import plotly.express as px
 from tools.prep import dict_to_frame
-from tools.exceptions import MissInputData
 
-# TODO: Сделать нейминг столбцов единообразным для весх атрибуций
-def last_click(df, path_col='path', conv_col='conversion', plot=True):
+
+def last_click(df, path_col='path', conv_col='conversion', sep='^', plot=True):
     """
     Preforms last-click attribution model
     :param
@@ -17,60 +16,52 @@ def last_click(df, path_col='path', conv_col='conversion', plot=True):
     - pandas.DataFrame with channels an their attribution score (not-normalized)
     """
     df_ = df.copy()
-    df_['first'] = df_[path_col].apply(lambda x: x.split('^')[-1])
+    new_name = path_col+'_new'
+    df_[new_name] = df_[path_col].apply(lambda x: x.split(sep)[-1])
 
-    plt_data = df_.groupby('first', as_index = False).agg({conv_col : 'sum'})
+    plt_data = df_.groupby(new_name, as_index = False).agg({conv_col : 'sum'})
     plt_data[conv_col+'_rel'] = plt_data[conv_col] / plt_data[conv_col].sum()
 
-
     if plot:
-        fig = px.bar(plt_data, y = 'first', x = conv_col+'_rel', title = 'Last Click Model', orientation = 'h')
-        fig.update_xaxes(title_text = 'Доля от всех конверсий')
-        fig.update_yaxes(title_text = 'Группа каналов')
+        fig = px.bar(plt_data, y=new_name, x=conv_col+'_rel', title='Last Click Model', orientation='h')
+        fig.update_xaxes(title_text='Доля от всех конверсий')
+        fig.update_yaxes(title_text='Группа каналов')
         fig.show()
 
     return plt_data
 
 
-# TODO: Переписать Last-non-direct-click для данных помимо мазды
-def last_non_direct_click(df):
-    not_direct = df.query('channel_grouping != "Прямой" and session_with_conversion == True \
-    and post_click_or_post_view == "click"')
-    direct = df.query('channel_grouping == "Прямой" and session_with_conversion == True')
-    dir_cid = direct['clientId'].unique()
+def last_non_direct_click(df, path_col='path', conv_col='conversion', sep='^', direct_name=None, plot=True):
 
-    _ = (df
-        .query('''session_with_conversion == False and post_click_or_post_view == "click" \
-               and channel_grouping != "Прямой" and clientId in @dir_cid''')
-        .groupby('clientId', as_index = False)
-        .agg({'time' : 'max'})
-       )
+    direct_name = 'Direct' if direct_name is None else direct_name
 
-    test = (df
-            .merge(_, how = 'left', left_on = 'clientId', right_on = 'clientId').query('time_x == time_y')
-            .drop('time_y', axis = 1)
-            .rename(columns = {'time_x' : 'time'})
-            .query('post_click_or_post_view == "click"')
-           )
+    def non_direct_index(path_list, direct_name=direct_name):
+        # reverse path list cause we are looking from last interaction
+        path_list.reverse()
+        idx = next((x[0] for x in enumerate(path_list) if x[1] != direct_name), None)
+        if idx is None:
+            return direct_name
 
+        return path_list[idx]
 
+    new_name = path_col + '_new'
+    df_ = df.copy()
+    df_[new_name] = df[path_col]\
+        .apply(lambda x: x.split(sep))\
+        .apply(lambda x: non_direct_index(x, direct_name))
 
-    res = pd.concat([not_direct, test])
-    res = pd.concat([res, direct[~direct['clientId'].isin(test['clientId'].unique())]])
+    plt_data = df_.groupby(new_name, as_index=False).agg({conv_col: 'sum'})
 
-    plt_data = res.groupby('channel_grouping', as_index = False).agg({'clientId' : 'nunique'})
+    if plot:
+        fig = px.bar(plt_data, y=new_name, x=conv_col, title='Last non-direct click Model', orientation='h')
+        fig.update_xaxes(title_text='Доля от всех конверсий')
+        fig.update_yaxes(title_text='Группа каналов')
+        fig.show()
 
-    plt_data['clientId'] = plt_data['clientId'] / plt_data['clientId'].sum()
-
-    fig = px.bar(plt_data, y = 'channel_grouping', x = 'clientId', title = 'Last non-direct click Model', orientation = 'h')
-    fig.update_xaxes(title_text = 'Доля от всех конверсий')
-    fig.update_yaxes(title_text = 'Группа каналов')
-    fig.show()
-
-    return
+    return plt_data
 
 
-def first_click(df, path_col = 'path', conv_col='conversion', plot=True):
+def first_click(df, path_col='path', conv_col='conversion', sep='^', plot=True):
     """
     Preforms first-click attribution model
     :param
@@ -84,22 +75,22 @@ def first_click(df, path_col = 'path', conv_col='conversion', plot=True):
     """
 
     df_ = df.copy()
-    df_['first'] = df_[path_col].apply(lambda x: x.split('^')[0])
+    new_name = path_col + '_new'
+    df_[new_name] = df_[path_col].apply(lambda x: x.split(sep)[0])
 
-    plt_data = df_.groupby('first', as_index = False).agg({conv_col : 'sum'})
+    plt_data = df_.groupby(new_name, as_index = False).agg({conv_col : 'sum'})
     plt_data[conv_col+'_rel'] = plt_data[conv_col] / plt_data[conv_col].sum()
 
-
     if plot:
-        fig = px.bar(plt_data, y = 'first', x = conv_col+'_rel', title = 'First click model', orientation = 'h')
-        fig.update_xaxes(title_text = 'Доля от всех конверсий')
-        fig.update_yaxes(title_text = 'Группа каналов')
+        fig = px.bar(plt_data, y=new_name, x=conv_col+'_rel', title='First click model', orientation='h')
+        fig.update_xaxes(title_text='Доля от всех конверсий')
+        fig.update_yaxes(title_text='Группа каналов')
         fig.show()
 
     return plt_data
 
 
-def uniform(df, unique_channels, path_col = 'path', conv_col='conversion', plot=True, as_frame=False,
+def uniform(df, unique_channels, path_col='path', conv_col='conversion', plot=True, as_frame=False,
             keys_col_name=None, values_col_name=None):
     """
     Preforms last-click attribution model
@@ -120,31 +111,28 @@ def uniform(df, unique_channels, path_col = 'path', conv_col='conversion', plot=
     d = {}
     for channel in unique_channels:
         df_[channel] = df_[path_col].apply(lambda x: channel in x)
-#         df_[channel + '_len'] =
 
         d[channel] = df_[df_[channel] == True][conv_col].sum()
 
-    d_ = {k : v/sum(d.values()) for k, v in zip(d.keys(), d.values())}
+    d_ = {k: v/sum(d.values()) for k, v in zip(d.keys(), d.values())}
 
     if plot:
-        fig = px.bar(y = d_.keys(), x = d_.values(), title = 'Uniform model', orientation = 'h')
+        fig = px.bar(y = d_.keys(), x=d_.values(), title='Uniform model', orientation='h')
         fig.update_xaxes(title_text = 'Доля от всех конверсий')
         fig.update_yaxes(title_text = 'Группа каналов')
         fig.show()
 
     if as_frame == True:
-        try:
-            if keys_col_name is not None and values_col_name is not None:
-                return dict_to_frame(d, keys_col_name, values_col_name)
-            else:
-                raise MissInputData
-        except MissInputData:
-            print("When as_frame is set to True keys_col_name and values_col_name must be specified")
+        if keys_col_name is not None and values_col_name is not None:
+            return dict_to_frame(d, keys_col_name, values_col_name)
+        else:
+            keys_col_name = path_col + '_new'
+            values_col_name = conv_col
+            return dict_to_frame(d, keys_col_name, values_col_name)
 
 
-
-def time_decay(df, unique_channels, path_col='path', path_len='path_len', conv_col='conversion', as_frame=False,
-               plot=True, recent=True, keys_col_name=None, values_col_name=None):
+def time_decay(df, unique_channels, path_col='path', path_len='path_len', conv_col='conversion', sep='^',
+               as_frame=False, plot=True, recent=True, keys_col_name=None, values_col_name=None):
     """
     Preforms time decay attribution model
     when recent = True then more recent channel is more valuable
@@ -165,7 +153,7 @@ def time_decay(df, unique_channels, path_col='path', path_len='path_len', conv_c
     for path, path_len, conversions in zip(df[path_col], df[path_len], df[conv_col]):
         L = path_len
 
-        cur_path = path.split('^')
+        cur_path = path.split(sep)
         if recent:
             for channel in cur_path:
                 d[channel] += int(conversions / L)
@@ -183,18 +171,17 @@ def time_decay(df, unique_channels, path_col='path', path_len='path_len', conv_c
         fig.show()
 
     if as_frame == True:
-        try:
-            if keys_col_name is not None and values_col_name is not None:
-                return dict_to_frame(d, keys_col_name, values_col_name)
-            else:
-                raise MissInputData
-        except MissInputData:
-            print("When as_frame is set to True keys_col_name and values_col_name must be specified")
+        if keys_col_name is not None and values_col_name is not None:
+            return dict_to_frame(d, keys_col_name, values_col_name)
+        else:
+            keys_col_name = path_col + '_new'
+            values_col_name = conv_col
+            return dict_to_frame(d, keys_col_name, values_col_name)
 
     return d
 
 
-def position(df, unique_channels, path_col='path', conv_col='conversion', plot=True, as_frame=False,
+def position(df, unique_channels, path_col='path', conv_col='conversion', sep='^', plot=True, as_frame=False,
              keys_col_name=None, values_col_name=None):
     """
     Preforms position_based attribution model
@@ -215,7 +202,7 @@ def position(df, unique_channels, path_col='path', conv_col='conversion', plot=T
     d = {channel : 0 for channel in unique_channels}
 
     for path, conversions in zip(df[path_col], df[conv_col]):
-        cur_path = path.split('^')
+        cur_path = path.split(sep)
         L = len(cur_path)
 
         for pos in range(L):
@@ -236,13 +223,12 @@ def position(df, unique_channels, path_col='path', conv_col='conversion', plot=T
             fig.show()
 
     if as_frame == True:
-        try:
-            if keys_col_name is not None and values_col_name is not None:
-                return dict_to_frame(d, keys_col_name, values_col_name)
-            else:
-                raise MissInputData
-        except MissInputData:
-            print("When as_frame is set to True keys_col_name and values_col_name must be specified")
+        if keys_col_name is not None and values_col_name is not None:
+            return dict_to_frame(d, keys_col_name, values_col_name)
+        else:
+            keys_col_name = path_col + '_new'
+            values_col_name = conv_col
+            return dict_to_frame(d, keys_col_name, values_col_name)
 
     return d
 
